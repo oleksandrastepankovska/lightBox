1ecb37d40d420e559da41bf8f675e17c228592bf
saskasaska <oldstep13@gmail.com>
2021-01-09 16:46:03 +0200

    add image count

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const images = document.querySelectorAll("img");
 const img = document.createElement("img");
+let imageIndex = document.createElement("div");
+imageIndex.className = "imageIndex";
 const lightbox = document.createElement("div");
 lightbox.className = "lightbox";
 document.body.appendChild(lightbox);
 
-images.forEach(image => {
+images.forEach((image, index) => {
     image.addEventListener("click", e => {lightbox.classList.add('active') 
     img.src = image.src;
     img.className = "lightboxImage";
-    lightbox.appendChild(img);  
+    img.index = index+1;
+    imageIndex.innerHTML = "Image " + img.index + " of " + images.length;
+    lightbox.appendChild(img);
+    lightbox.appendChild(imageIndex);
     })
 })
 
